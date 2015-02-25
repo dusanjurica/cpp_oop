99d2740bc74837750670ccdd5ed956666b226456
Dušan Jurica <dusanjurica@Dusan-MacBook-Pro.local>
2015-02-25 22:52:13 +0100

    Initial Commit

---
+++ mypg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mypg
+//
+//  Created by Dušan Jurica on 25.02.15.
+//  Copyright (c) 2015 Dušan Jurica. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
