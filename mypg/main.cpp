95be47394165c5d15861fe731b49ce47aaad5187
Dušan Jurica <dusanjurica@Dusan-MacBook-Pro.local>
2015-02-25 23:18:14 +0100

    Pokusy s enum

--- mypg/main.cpp
+++ mypg/main.cpp
@@ -6,11 +6,25 @@
 //  Copyright (c) 2015 Dušan Jurica. All rights reserved.
 //
 
+class base_header{
+public:
+    enum t_delta {key0, key1, key2, key5=5, key10=10, key20=20} delta;
+};
+
 #include <iostream>
 #include <ConditionalMacros.h>
 
+using namespace std;
+
+base_header inst;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
+    
+    inst.delta = inst.key10;
+    
+    cout << inst.delta;
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
